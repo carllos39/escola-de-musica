c32e81141867124385d7776e39180574210bf568
carllos39 <carllos2005@outlook.com.br>
2025-01-31 12:57:58 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-  
-  // Slider Automático
-  const slides = document.querySelector(".slides");
-  let index = 0;
+const mudarCor = document.querySelector("h1");
+mudarCor.addEventListener("mouseover", () => {
+  mudarCor.style.backgroundColor = "green";
+  mudarCor.style.color = "white";
+});
 
-  function mudarSlide() {
-      index = (index + 1) % 4; // calcula o total de 3 slides
-      slides.style.transform = `translateX(-${index * 100}%)`;
-  }
+mudarCor.addEventListener("mouseout", () => {
+  mudarCor.style.backgroundColor = "";
+  mudarCor.style.color = "";
+});
 
-  setInterval(mudarSlide, 4000);
+
+
+// Slider Automático
+const slides = document.querySelector(".slides");
+let index = 0;
+
+function mudarSlide() {
+  index = (index + 1) % 4; // calcula o total de 3 slides
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(mudarSlide, 4000);
