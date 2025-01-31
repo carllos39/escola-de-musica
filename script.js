569360caeba0c3f37179c0860fcff83b160a667c
carllos39 <140573083+carllos39@users.noreply.github.com>
2025-01-30 21:59:31 -0300

    30/01/2025

---
+++ script.js
@@ -0,0 +1,10 @@
+  // Slider Automático
+  const slides = document.querySelector(".slides");
+  let index = 0;
+
+  function mudarSlide() {
+      index = (index + 1) % 4; // calcula o total de 3 slides
+      slides.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  setInterval(mudarSlide, 4000);
