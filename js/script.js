d1c51ab48739deb045b742219567742e00fa17b9
Morais <moraislcs05@gmail.com>
2025-02-04 21:47:10 -0300

    alterando carrossel, imagens e botao de cursos

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
 
-const mudarCor = document.querySelector("h1");
-const botao=document.getElementById("botao");
-const lista=document.getElementById("lista");
+  const mudarCor = document.querySelector("h1");
+  const botao = document.getElementById("botao");
+  const lista = document.getElementById("lista");
 
-mudarCor.addEventListener("mouseover", () => {
-  mudarCor.style.backgroundColor = "blue";
-  mudarCor.style.color = "white";
-});
+  mudarCor.addEventListener("mouseover", () => {
+    mudarCor.style.backgroundColor = "blue";
+    mudarCor.style.color = "white";
+  });
 
-mudarCor.addEventListener("mouseout", () => {
-  mudarCor.style.backgroundColor = "";
-  mudarCor.style.color = "";
-});
-botao.addEventListener("click", () => {
-  lista.style.display = lista.style.display === "block" ? "none" : "block";
-});
+  mudarCor.addEventListener("mouseout", () => {
+    mudarCor.style.backgroundColor = "";
+    mudarCor.style.color = "";
+  });
+
+  botao.addEventListener("click", () => {
+    lista.style.display = lista.style.display === "block" ? "none" : "block";
+  });
 
 
 });
@@ -27,9 +28,8 @@ const slides = document.querySelector(".slides");
 let index = 0;
 
 function mudarSlide() {
-  index = (index + 1) % 4; // calcula o total de 3 slides
+  index = (index + 1) % 4;
   slides.style.transform = `translateX(-${index * 100}%)`;
 }
 
 setInterval(mudarSlide, 4000);
-
